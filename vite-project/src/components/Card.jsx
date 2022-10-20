c43ee6eadc605daf2b4966bbb1439dd83cf95bbc
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-20 11:49:08 +0200

    add and style sample card with img and info

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -2,6 +2,16 @@ import React from "react";
 
 export default function Card() {
     return (
-        
+        <div className="card--div">
+            <img src="./src/images/katie-zaferes.png"/>
+            <div>
+                <img src="./src/images/star.png"/>
+                <p>5.0</p>
+                <p className="gray">(6) |</p>
+                <p className="gray"> USA</p>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
     )
 }
