e65cd4a160f25bd119e1c35f466d928f3f4fc274
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-22 13:11:11 +0200

    clear props from app.jsx so it is cleaner

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 export default function Card(props) {
     let badgeText 
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "Online"
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`/public/images/${props.img}`} className="card--image"/>
+            <img src={`/public/images/${props.item.coverImg}`} className="card--image"/>
             <div className="card--stats">
                 <img src="/public/images/star.png" className="card--star"/>
-                <p>{props.rating}</p>
-                <p className="gray">({props.reviewCount}) |</p>
-                <p className="gray">{props.location}</p>
+                <p>{props.item.stats.rating}</p>
+                <p className="gray">({props.item.stats.reviewCount}) |</p>
+                <p className="gray">{props.item.location}</p>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">{props.price}$</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">{props.item.price}$</span> / person</p>
         </div>
     )
 }
