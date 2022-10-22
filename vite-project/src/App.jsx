7132c8ce5a7baab5569eec2426e56f205e1fb8f3
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-22 12:05:39 +0200

    new images and mapped card data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,20 +3,37 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data.jsx'
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 export default function App() {
-              {/* <Hero /> */}
+  const cardElements = data.map(data => {
+    return (<Card 
+      img={data.coverImg}
+      rating={data.stats.rating}
+      reviewCount={data.stats.reviewCount}
+      location={data.location}
+      title={data.title}
+      price={data.price}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
-      <Card 
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount="6"
-        country="Spain"
-        title="Test 1 of props usage"
-        price="136"
-      />
+    {/* <Hero /> */}
+      {cardElements}
     </div>
   )
 }
--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Card(props) {
     return (
         <div className="card--div">
-            <img src={`/public/images/${props.img}`}/>
+            <img src={`/public/images/${props.coverImg}`}/>
             <div>
                 <img src="/public/images/star.png"/>
                 <p>{props.rating}</p>
                 <p className="gray">({props.reviewCount}) |</p>
-                <p className="gray">{props.country}</p>
+                <p className="gray">{props.location}</p>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">{props.price}$</span> / person</p>
