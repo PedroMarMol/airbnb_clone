ec6268e50ed46fa0bfa63a0b0bed8d7cc66afa9f
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-22 11:49:04 +0200

    move images dir and style airbnb logo

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Card(props) {
     return (
         <div className="card--div">
-            <img src={`/src/images/${props.img}`}/>
+            <img src={`/public/images/${props.img}`}/>
             <div>
-                <img src="/src/images/star.png"/>
+                <img src="/public/images/star.png"/>
                 <p>{props.rating}</p>
                 <p className="gray">({props.reviewCount}) |</p>
                 <p className="gray">{props.country}</p>
